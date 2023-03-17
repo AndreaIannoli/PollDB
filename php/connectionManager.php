fbf2bb4fc8dd7306176532acbc4fd9c97eb08740
Andrea Iannoli <andreaiannoli@Andreas-MBP.station>
2023-03-17 15:19:53 +0100

    DB script update

---
+++ php/connectionManager.php
@@ -0,0 +1,14 @@
+<?php
+    $host = "localhost";
+    $dbName = "PollDB";
+    $username = "root";
+    $pass = "root";
+    try {
+        $pdo = new PDO('mysql:host='.$host.';dbname='.$dbName, $username, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        echo("[ERRORE] Connessione al DB non riuscita. Errore: " . $e->getMessage());
+        exit();
+    }
+?>
